d8ef24a7a0f77ac277e50ab1857bbb65a97bd262
Jedrzej <jpt.graczykowski@gmail.com>
2025-02-17 09:57:26 +0000

    08 prepare empty flashcard_set page

--- app/controllers/flashcard_sets_controller.rb
+++ app/controllers/flashcard_sets_controller.rb
@@ -1,5 +1,26 @@
 class FlashcardSetsController < ApplicationController
+  layout :resolve_layout
+  before_action :set_flashcard_set, only: [:show]
+
   def index
     @flashcard_sets = FlashcardSet.all
+  end
+
+  def show
+    @flashcards = @flashcard_set.flashcards
+  end
+
+  private
+
+  def set_flashcard_set
+    @flashcard_set = FlashcardSet.find(params[:id])
+  end
+
+  def resolve_layout
+    if action_name == "index"
+      "application"
+    else
+      "application_with_sidebar"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "flashcard_sets#index"
+
+  resources :flashcard_sets, only: [:index, :show]
 end
