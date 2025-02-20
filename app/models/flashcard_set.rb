d9b101c4e6b53e2ea24f3479fbe3aee013d5ec3b
Jedrzej <jpt.graczykowski@gmail.com>
2025-02-20 15:49:49 +0000

    26 make it possible to delete flashcard sets

--- app/models/flashcard_set.rb
+++ app/models/flashcard_set.rb
@@ -1,5 +1,5 @@
 class FlashcardSet < ApplicationRecord
-  has_many :flashcards, -> { order(position: :asc) }
+  has_many :flashcards, -> { order(position: :asc) }, dependent: :destroy
 
   validates :english_name, :polish_name, presence: true
   validates :english_name, :polish_name, uniqueness: true
