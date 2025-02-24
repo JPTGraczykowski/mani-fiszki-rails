88670fcf080530b68b2be155abb484538e1ca66c
Jedrzej <jpt.graczykowski@gmail.com>
2025-02-24 13:42:24 +0000

    35 add shadows

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,18 +12,18 @@ module ApplicationHelper
   end
 
   def back_button_classes
-    "w-18 rounded-lg border border-gray-300 bg-lime-200 px-1 text-lg #{hover_classes}"
+    "w-18 rounded-lg border shadow-md border-gray-300 bg-lime-200 px-1 text-lg #{hover_classes}"
   end
 
   def input_classes
-    "block rounded-lg border border-gray-300 focus:outline-gray-600 px-3 py-2 w-full bg-white"
+    "block rounded-lg border shadow-md border-gray-300 focus:outline-gray-600 px-3 py-2 w-full bg-white"
   end
 
   def submit_button_classes
-    "rounded-md px-3.5 py-2 bg-lime-200 inline-block font-medium cursor-pointer border border-gray-300 text-center #{hover_classes}"
+    "rounded-md px-3.5 py-2 bg-lime-200 inline-block font-medium cursor-pointer border shadow-md border-gray-300 text-center #{hover_classes}"
   end
 
   def secondary_button_classes
-    "rounded-md px-3.5 py-2 bg-white inline-block font-medium cursor-pointer border border-gray-300 text-center #{hover_classes}"
+    "rounded-md px-3.5 py-2 bg-white inline-block font-medium cursor-pointer border shadow-md border-gray-300 text-center #{hover_classes}"
   end
 end
