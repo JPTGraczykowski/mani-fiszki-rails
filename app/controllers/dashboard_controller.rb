8aa848104b5d9a9c082e3facd70c4572e101d7eb
Jedrzej <jpt.graczykowski@gmail.com>
2025-03-27 18:03:17 +0000

    48 add dashboard to choose between flashcards and sentences

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,7 @@
+class DashboardController < ApplicationController
+  allow_unauthenticated_access
+  layout "application"
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   put "toggle_language" => "preferred_languages#toggle_language"
 
-  root "flashcard_sets#index"
+  root "dashboard#show"
 
   resources :flashcard_sets, only: [:index, :show] do
     resources :flashcards, only: [:index] do
