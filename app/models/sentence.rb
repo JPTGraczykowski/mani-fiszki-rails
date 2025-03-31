f08aa157357d1a5e711f5a83deca7199a85ad7a0
Jedrzej <jpt.graczykowski@gmail.com>
2025-03-31 09:58:19 +0000

    51 show sentence words with the fake ones

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -1,6 +1,6 @@
 class Sentence < ApplicationRecord
   belongs_to :sentence_set, required: true
-  has_many :words, dependent: :destroy
+  has_many :words, -> { order(:position) }, dependent: :destroy
   has_many :fake_words, dependent: :destroy
 
   acts_as_list scope: :sentence_set
--- app/models/word.rb
+++ app/models/word.rb
@@ -4,4 +4,6 @@ class Word < ApplicationRecord
   acts_as_list scope: :sentence
 
   validates :value, presence: true
+
+  scope :visible, -> { where(missing: false) }
 end
